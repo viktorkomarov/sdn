6762e26a6463136b7b08898ce85c7836e3dbe48e
Viktor Komarov <victorkomarov98@mail.ru>
2021-07-17 17:01:49 +0300

    small update

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
+	"errors"
 	"math"
-	"strings"
-	"unicode"
 )
 
 // E =  F | H | S H | S F
@@ -13,75 +11,30 @@ import (
 // S =  !
 // D =  & | '|' | > | - | +
 
-type Token int
-
-const (
-	Variable Token = iota
-	SingleOp
-	DoubleOp
-	OpenBracket
-	CloseBracket
-)
-
-type TokenVal struct {
-	Typ Token
-	Val rune
-}
-
-func tokenize(expr string) ([]TokenVal, error) {
-	tokens := make([]TokenVal, 0)
-
-	for _, symbol := range strings.ReplaceAll(expr, " ", "") {
-		var token Token
-		switch symbol {
-		case '(':
-			token = OpenBracket
-		case ')':
-			token = CloseBracket
-		case '!':
-			token = SingleOp
-		case '&', '|', '>', '-', '+':
-			token = DoubleOp
-		default:
-			if !unicode.IsUpper(symbol) || !unicode.IsLetter(symbol) {
-				return nil, fmt.Errorf("unknown token %s", string(symbol))
-			}
-
-			token = Variable
-		}
-
-		tokens = append(tokens, TokenVal{
-			Typ: token,
-			Val: symbol,
-		})
-	}
-
-	return tokens, nil
-}
-
 type Expression struct {
 	variables map[rune]bool
 	executor  Executor
 }
 
-// func Parse(expr string) (Expression, error) {
-// 	tokens, err := tokenize(expr)
-// 	if err != nil {
-// 		return Expression{}, err
-// 	}
+var ErrEmptyTokens = errors.New("empty tokens")
 
-// 	variables := make(map[rune]bool)
-// 	for _, token := range tokens {
-// 		if token.Typ == Variable {
-// 			variables[token.Val] = true
-// 		}
-// 	}
+func parseExpr(tokens []TokenVal) (Expr, error) {
+	if len(tokens) == 0 {
+		return Expr{}, ErrEmptyTokens
+	}
+}
 
-// 	return Expression{
-// 		variables: variables,
-// 		tokens:    tokens,
-// 	}, nil
-// }
+func Parse(str string) (Expression, error) {
+	tokens := tokenize(str)
+	variables := make(map[rune]bool)
+	for _, token := range tokens {
+		if token.Typ == Variable {
+			variables[token.Val] = true
+		}
+	}
+
+	parseExpr(tokens)
+}
 
 type Row struct {
 	values map[rune]bool
